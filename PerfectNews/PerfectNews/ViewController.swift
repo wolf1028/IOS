fc5c83749666cff3ca9fa5b487ce8fc410fdab38
Alumno-DG <Alumno-DG@B303-08.local>
2017-09-19 19:52:33 -0500

    Inicio del proyecto

---
+++ PerfectNews/PerfectNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PerfectNews
+//
+//  Created by Alumno-DG on 19/09/17.
+//  Copyright © 2017 Alumno-DG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
